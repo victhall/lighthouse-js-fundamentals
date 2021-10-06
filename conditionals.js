4039155323fce035e164d2f3b42b2f06a04ca962
Victoria Hall <victoriahall.dev@gmail.com>
2021-10-06 19:27:20 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -16,3 +16,9 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
